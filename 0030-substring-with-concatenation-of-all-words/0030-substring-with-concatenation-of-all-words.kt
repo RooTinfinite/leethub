01519b58400ad1289ad15279c2090e03d46ac991
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 05:02:05 +0200

    Time: 217 ms (100.00%), Space: 37.1 MB (96.30%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.kt
@@ -0,0 +1,43 @@
+class Solution {
+    fun findSubstring(s: String, words: Array<String>): List<Int> {
+        val indices: MutableList<Int> = ArrayList()
+        if (words.size == 0) {
+            return indices
+        }
+        // Put each word into a HashMap and calculate word frequency
+        val wordMap: MutableMap<String, Int> = HashMap()
+        for (word in words) {
+            wordMap[word] = wordMap.getOrDefault(word, 0) + 1
+        }
+        val wordLength = words[0].length
+        val window = words.size * wordLength
+        for (i in 0 until wordLength) {
+            // move a word's length each time
+            var j = i
+            while (j + window <= s.length) {
+
+                // get the subStr
+                val subStr = s.substring(j, j + window)
+                val map: MutableMap<String, Int> = HashMap()
+                // start from the last word
+                for (k in words.indices.reversed()) {
+                    // get the word from subStr
+                    val word = subStr.substring(k * wordLength, (k + 1) * wordLength)
+                    val count = map.getOrDefault(word, 0) + 1
+                    // if the num of the word is greater than wordMap's, move (k * wordLength) and
+                    // break
+                    if (count > wordMap.getOrDefault(word, 0)) {
+                        j = j + k * wordLength
+                        break
+                    } else if (k == 0) {
+                        indices.add(j)
+                    } else {
+                        map[word] = count
+                    }
+                }
+                j = j + wordLength
+            }
+        }
+        return indices
+    }
+}
