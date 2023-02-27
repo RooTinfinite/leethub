ea5cd65e07f52eb078cebf346fa078c7bb094b03
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 05:54:18 +0200

    Time: 171 ms (100.00%), Space: 35.6 MB (98.18%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.kt
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque
+import java.util.Deque
+
+class Solution {
+    fun simplifyPath(path: String): String {
+        val stk: Deque<String> = ArrayDeque()
+        var start = 0
+        while (start < path.length) {
+            while (start < path.length && path[start] == '/') {
+                start++
+            }
+            var end = start
+            while (end < path.length && path[end] != '/') {
+                end++
+            }
+            val s = path.substring(start, end)
+            if (s == "..") {
+                if (!stk.isEmpty()) {
+                    stk.pop()
+                }
+            } else if (s != "." && s != "") {
+                stk.push(s)
+            }
+            start = end + 1
+        }
+        val ans = StringBuilder()
+        while (!stk.isEmpty()) {
+            ans.insert(0, stk.pop())
+            ans.insert(0, "/")
+        }
+        return if (ans.length > 0) ans.toString() else "/"
+    }
+}
