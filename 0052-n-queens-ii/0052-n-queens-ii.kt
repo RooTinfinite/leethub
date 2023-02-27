9d37458b53787e33522570fb9d9ae0e22cf30fd2
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 05:40:11 +0200

    Time: 136 ms (100.00%), Space: 33.4 MB (82.86%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.kt
@@ -0,0 +1,37 @@
+class Solution {
+    fun totalNQueens(n: Int): Int {
+        val row = BooleanArray(n)
+        val col = BooleanArray(n)
+        val diagonal = BooleanArray(n + n - 1)
+        val antiDiagonal = BooleanArray(n + n - 1)
+        return totalNQueens(n, 0, row, col, diagonal, antiDiagonal)
+    }
+
+    private fun totalNQueens(
+        n: Int,
+        r: Int,
+        row: BooleanArray,
+        col: BooleanArray,
+        diagonal: BooleanArray,
+        antiDiagonal: BooleanArray
+    ): Int {
+        if (r == n) {
+            return 1
+        }
+        var count = 0
+        for (c in 0 until n) {
+            if (!row[r] && !col[c] && !diagonal[r + c] && !antiDiagonal[r - c + n - 1]) {
+                antiDiagonal[r - c + n - 1] = true
+                diagonal[r + c] = antiDiagonal[r - c + n - 1]
+                col[c] = diagonal[r + c]
+                row[r] = col[c]
+                count += totalNQueens(n, r + 1, row, col, diagonal, antiDiagonal)
+                antiDiagonal[r - c + n - 1] = false
+                diagonal[r + c] = antiDiagonal[r - c + n - 1]
+                col[c] = diagonal[r + c]
+                row[r] = col[c]
+            }
+        }
+        return count
+    }
+}
