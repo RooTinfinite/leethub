30425c59af10568e187a1541858ab41b3f85af9d
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 05:55:49 +0200

    Time: 159 ms (100.00%), Space: 36.5 MB (98.29%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun minWindow(s: String, t: String): String {
+        val map = IntArray(128)
+        for (element in t) {
+            map[element - 'A']++
+        }
+        var count = t.length
+        var begin = 0
+        var end = 0
+        var d = Int.MAX_VALUE
+        var head = 0
+        while (end < s.length) {
+            if (map[s[end++] - 'A']-- > 0) {
+                count--
+            }
+            while (count == 0) {
+                if (end - begin < d) {
+                    d = end - begin
+                    head = begin
+                }
+                if (map[s[begin++] - 'A']++ == 0) {
+                    count++
+                }
+            }
+        }
+        return if (d == Int.MAX_VALUE) "" else s.substring(head, head + d)
+    }
+}
