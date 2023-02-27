336d2864a4bc5ee10752b02015dda9d5e2d1ee26
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 05:39:55 +0200

    Time: 202 ms (95.45%), Space: 36.6 MB (97.73%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.kt
@@ -0,0 +1,41 @@
+class Solution {
+    fun solveNQueens(n: Int): List<List<String>> {
+        val pos = BooleanArray(n + 2 * n - 1 + 2 * n - 1)
+        val pos2 = IntArray(n)
+        val ans: MutableList<List<String>> = ArrayList()
+        helper(n, 0, pos, pos2, ans)
+        return ans
+    }
+
+    private fun helper(n: Int, row: Int, pos: BooleanArray, pos2: IntArray, ans: MutableList<List<String>>) {
+        if (row == n) {
+            construct(n, pos2, ans)
+            return
+        }
+        for (i in 0 until n) {
+            val index = n + 2 * n - 1 + n - 1 + i - row
+            if (pos[i] || pos[n + i + row] || pos[index]) {
+                continue
+            }
+            pos[i] = true
+            pos[n + i + row] = true
+            pos[index] = true
+            pos2[row] = i
+            helper(n, row + 1, pos, pos2, ans)
+            pos[i] = false
+            pos[n + i + row] = false
+            pos[index] = false
+        }
+    }
+
+    private fun construct(n: Int, pos: IntArray, ans: MutableList<List<String>>) {
+        val sol: MutableList<String> = ArrayList()
+        for (r in 0 until n) {
+            val queenRow = CharArray(n)
+            Arrays.fill(queenRow, '.')
+            queenRow[pos[r]] = 'Q'
+            sol.add(String(queenRow))
+        }
+        ans.add(sol)
+    }
+}
