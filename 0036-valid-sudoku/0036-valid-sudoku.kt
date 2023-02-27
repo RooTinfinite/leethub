37f13070de375834422c006a26f1b7a506233753
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 05:04:12 +0200

    Time: 174 ms (100.00%), Space: 36.1 MB (98.46%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.kt
@@ -0,0 +1,41 @@
+class Solution {
+    private var j1 = 0
+    private val i1 = IntArray(9)
+    private val b1 = IntArray(9)
+
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        for (i in 0..8) {
+            for (j in 0..8) {
+                val res = checkValid(board, i, j)
+                if (!res) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun checkValid(board: Array<CharArray>, i: Int, j: Int): Boolean {
+        if (j == 0) {
+            j1 = 0
+        }
+        if (board[i][j] == '.') {
+            return true
+        }
+        val `val` = board[i][j] - '0'
+        if (j1 == j1 or (1 shl `val` - 1)) {
+            return false
+        }
+        j1 = j1 or (1 shl `val` - 1)
+        if (i1[j] == i1[j] or (1 shl `val` - 1)) {
+            return false
+        }
+        i1[j] = i1[j] or (1 shl `val` - 1)
+        val b = i / 3 * 3 + j / 3
+        if (b1[b] == b1[b] or (1 shl `val` - 1)) {
+            return false
+        }
+        b1[b] = b1[b] or (1 shl `val` - 1)
+        return true
+    }
+}
