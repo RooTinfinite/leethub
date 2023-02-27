7aeaa87a29802f09b8fae9856e43c84e2de6174a
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 04:52:55 +0200

    Time: 212 ms (98.02%), Space: 35.5 MB (66.43%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) return false
+        var rev = 0
+        var localX = x
+        while (localX > 0) {
+            rev *= 10
+            rev += localX % 10
+            localX /= 10
+        }
+        return rev == x
+    }
+}
