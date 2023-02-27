0c7179b4cbc62ace90d7b9385797678a266cc3bb
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 04:49:50 +0200

    Time: 291 ms (70.68%), Space: 46.2 MB (65.59%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+        val l: MutableList<Int> = ArrayList()
+        val f: Double
+        for (j in nums1) {
+            l.add(j)
+        }
+        for (i in nums2) {
+            l.add(i)
+        }
+        l.sort()
+        val k = l.size
+        f = if (k % 2 == 0) {
+            (l[k / 2 - 1] + l[k / 2]).toDouble() / 2
+        } else {
+            l[(k + 1) / 2 - 1].toDouble()
+        }
+        return f
+    }
+}
