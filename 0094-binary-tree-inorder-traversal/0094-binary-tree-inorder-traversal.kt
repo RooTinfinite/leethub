db283c678527a1751258d7dcc7e1ecddb77cf2cc
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 07:51:23 +0200

    Time: 150 ms (97.69%), Space: 36.1 MB (8.97%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.kt
@@ -0,0 +1,33 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        if (root == null) {
+            return emptyList()
+        }
+        val answer: MutableList<Int> = ArrayList()
+        inorderTraversal(root, answer)
+        return answer
+    }
+
+    private fun inorderTraversal(root: TreeNode?, answer: MutableList<Int>) {
+        if (root == null) {
+            return
+        }
+        if (root.left != null) {
+            inorderTraversal(root.left, answer)
+        }
+        answer.add(root.`val`)
+        if (root.right != null) {
+            inorderTraversal(root.right, answer)
+        }
+    }
+}
