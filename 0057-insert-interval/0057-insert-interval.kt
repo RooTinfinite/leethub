83075b6e10037207ee620528fd23fdfc87e3cde4
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 05:49:02 +0200

    Time: 245 ms (92.95%), Space: 37.4 MB (91.85%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun insert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
+        val n = intervals.size
+        var l = 0
+        var r = n - 1
+        while (l < n && newInterval[0] > intervals[l][1]) {
+            l++
+        }
+        while (r >= 0 && newInterval[1] < intervals[r][0]) {
+            r--
+        }
+        val res = Array(l + n - r) { IntArray(2) }
+        for (i in 0 until l) {
+            res[i] = Arrays.copyOf(intervals[i], intervals[i].size)
+        }
+        res[l][0] = Math.min(newInterval[0], if (l == n) newInterval[0] else intervals[l][0])
+        res[l][1] = Math.max(newInterval[1], if (r == -1) newInterval[1] else intervals[r][1])
+        var i = l + 1
+        var j = r + 1
+        while (j < n) {
+            res[i] = intervals[j]
+            i++
+            j++
+        }
+        return res
+    }
+}
