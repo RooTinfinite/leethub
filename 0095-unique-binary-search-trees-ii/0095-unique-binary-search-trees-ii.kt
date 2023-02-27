a69661f0cedbe02256fccc62c4d60899979d4500
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 07:51:45 +0200

    Time: 187 ms (89.47%), Space: 37.3 MB (100.00%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.kt
@@ -0,0 +1,51 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun generateTrees(n: Int): List<TreeNode?> {
+        var result: MutableList<TreeNode?> = ArrayList<TreeNode?>()
+        result.add(TreeNode(1))
+        for (i in 2..n) {
+            val nresult: MutableList<TreeNode?> = ArrayList<TreeNode?>()
+            for (r in result) {
+                var node = TreeNode(i, r, null)
+                nresult.add(node)
+                var previous: TreeNode? = r
+                while (previous != null) {
+                    node = TreeNode(i)
+                    val cr: TreeNode? = copy(r)
+                    insert(cr, node, previous)
+                    previous = node.left
+                    nresult.add(cr)
+                }
+            }
+            result = nresult
+        }
+        return result
+    }
+
+    private fun insert(dest: TreeNode?, n: TreeNode, from: TreeNode) {
+        if (dest != null && dest.`val` == from.`val`) {
+            val h: TreeNode? = dest.right
+            dest.right = n
+            n.left = h
+            return
+        }
+        if (dest != null) {
+            insert(dest.right, n, from)
+        }
+    }
+
+    private fun copy(n: TreeNode?): TreeNode? {
+        return if (n == null) {
+            null
+        } else { TreeNode(n.`val`, copy(n.left), copy(n.right)) }
+    }
+}
