92a832e3c23563185fcd5b3c8a5e8541e721aa02
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-28 07:07:32 +0200

    Time: 194 ms (100.00%), Space: 37.3 MB (80.00%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.kt
@@ -0,0 +1,50 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun sortedListToBST(head: ListNode?): TreeNode? {
+        // Empty list -> empty tree / sub-tree
+        if (head == null) {
+            return null
+        }
+        // No next node -> this node will become leaf
+        if (head.next == null) {
+            val leaf = TreeNode(head.`val`)
+            leaf.left = null
+            leaf.right = null
+            return leaf
+        }
+        var slow = head
+        // Head-Start fast by 1 to get slow = mid -1
+        var fast = head.next!!.next
+        // Find the mid of list
+        while (fast != null && fast.next != null) {
+            slow = slow!!.next
+            fast = fast.next!!.next
+        }
+        // slow.next ->  mid = our "root"
+        val root = TreeNode(slow!!.next!!.`val`)
+        // Right sub tree from mid - end
+        root.right = sortedListToBST(slow.next!!.next)
+        // Left sub tree from head - mid (chop slow.next)
+        slow.next = null
+        root.left = sortedListToBST(head)
+        return root
+    }
+}
