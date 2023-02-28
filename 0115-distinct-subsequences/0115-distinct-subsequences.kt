bcfb14f1931df606a5369295db4a8b15468ffa49
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-28 07:11:28 +0200

    Time: 134 ms (100.00%), Space: 33.1 MB (100.00%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.kt
@@ -0,0 +1,42 @@
+class Solution {
+    fun numDistinct(s: String, t: String): Int {
+        if (s.length < t.length) {
+            return 0
+        }
+        if (s.length == t.length) {
+            return if (s == t) 1 else 0
+        }
+        val move = s.length - t.length + 2
+        // Only finite number of character in s can occupy first position in T. Same applies for
+        // every character in T.
+        val dp = IntArray(move)
+        var j = 1
+        var k = 1
+        for (i in 0 until t.length) {
+            var firstMatch = true
+            while (j < move) {
+                if (t[i] == s[i + j - 1]) {
+                    if (firstMatch) {
+                        // Keep track of first match. To avoid useless comparisons on next
+                        // iteration.
+                        k = j
+                        firstMatch = false
+                    }
+                    if (i == 0) {
+                        dp[j] = 1
+                    }
+                    dp[j] += dp[j - 1]
+                } else {
+                    dp[j] = dp[j - 1]
+                }
+                j++
+            }
+            // No match found for current character of t in s. No point in checking others.
+            if (dp[move - 1] == 0) {
+                return 0
+            }
+            j = k
+        }
+        return dp[move - 1]
+    }
+}
