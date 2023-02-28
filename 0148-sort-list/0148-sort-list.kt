5e9641f6c7f65578a4550217ff621b93eb1edfa9
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-28 07:47:18 +0200

    Time: 453 ms (62.16%), Space: 45.2 MB (79.73%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.kt
@@ -0,0 +1,55 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun sortList(head: ListNode?): ListNode? {
+        if (head == null || head.next == null) {
+            return head
+        }
+        // Step 1: split the list into halves
+        var prev: ListNode? = null
+        var slow = head
+        var fast = head
+        while (fast != null && fast.next != null) {
+            prev = slow
+            fast = fast.next!!.next
+            slow = slow!!.next
+        }
+        prev!!.next = null
+        // step 2: sort each half
+        val l1 = sortList(head)
+        val l2 = sortList(slow)
+        // step 3: merge the two halves
+        return merge(l1, l2)
+    }
+
+    private fun merge(l1: ListNode?, l2: ListNode?): ListNode? {
+        var l1 = l1
+        var l2 = l2
+        val result = ListNode(0)
+        var tmp: ListNode? = result
+        while (l1 != null && l2 != null) {
+            if (l1.`val` < l2.`val`) {
+                tmp!!.next = l1
+                l1 = l1.next
+            } else {
+                tmp!!.next = l2
+                l2 = l2.next
+            }
+            tmp = tmp.next
+        }
+        if (l1 != null) {
+            tmp!!.next = l1
+        }
+        if (l2 != null) {
+            tmp!!.next = l2
+        }
+        return result.next
+    }
+}
