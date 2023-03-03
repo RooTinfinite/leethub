a5e3eacc4fc98bdecc2929609531507cc3c06de9
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-03 07:21:20 +0200

    Time: 134 ms (97.01%), Space: 33.8 MB (70.90%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.kt
@@ -0,0 +1,36 @@
+class Solution {
+    fun rob(nums: IntArray): Int {
+        val n = nums.size
+        if (n == 0) {
+            return 0
+        }
+        if (n == 1) {
+            return nums[0]
+        }
+        if (n == 2) {
+            return Math.max(nums[0], nums[1])
+        }
+        if (n == 3) {
+            return Math.max(nums[0], Math.max(nums[1], nums[2]))
+        }
+        var max = Int.MIN_VALUE
+        val inc = IntArray(n)
+        val exc = IntArray(n)
+        inc[0] = nums[0]
+        exc[0] = 0
+        inc[1] = nums[0]
+        exc[1] = nums[1]
+        inc[2] = nums[2] + nums[0]
+        exc[2] = nums[2]
+        for (i in 3 until n - 1) {
+            inc[i] = Math.max(inc[i - 2], inc[i - 3]) + nums[i]
+            exc[i] = Math.max(exc[i - 2], exc[i - 3]) + nums[i]
+        }
+        inc[n - 1] = inc[n - 2]
+        exc[n - 1] = Math.max(exc[n - 3], exc[n - 4]) + nums[n - 1]
+        for (i in 0 until n) {
+            max = Math.max(max, Math.max(inc[i], exc[i]))
+        }
+        return max
+    }
+}
