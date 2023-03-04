f650b9c42333392ec72d0773a69abb209d074a92
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-04 08:40:59 +0200

    Time: 154 ms (60.81%), Space: 33.6 MB (51.35%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun addDigits(num: Int): Int {
+        if (num == 0) {
+            return 0
+        }
+        return if (num % 9 == 0) {
+            9
+        } else num % 9
+    }
+}
