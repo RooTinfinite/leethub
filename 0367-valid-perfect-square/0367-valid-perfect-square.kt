99fea5c549b58f603f9571c7d16fa3ee5acd0683
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-06 13:36:00 +0200

    Time: 119 ms (93.10%), Space: 32.9 MB (51.72%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun isPerfectSquare(num: Int): Boolean {
+        if (num == 0) {
+            // If num is 0 return false
+            return false
+        }
+        // long datatype can holds huge number.
+        var start: Long = 0
+        var end = num.toLong()
+        var mid: Long
+        while (start <= end) {
+            // until start is lesser or equal to end do this
+            // Finding middle value
+            mid = start + (end - start) / 2
+            if (mid * mid == num.toLong()) {
+                // if mid*mid == num return true
+                return true
+            } else if (mid * mid < num) {
+                // if num is greater than mid*mid then make start = mid + 1
+                start = mid + 1
+            } else if (mid * mid > num) {
+                // if num is lesser than mid*mid then make end = mid - 1
+                end = mid - 1
+            }
+        }
+        return false
+    }
+}
