ee8f687c19bd0f01dda3e6007c4adec95bfd2220
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-06 13:40:06 +0200

    Time: 736 ms (63.64%), Space: 108.1 MB (30.30%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.kt
@@ -0,0 +1,52 @@
+import kotlin.random.Random
+
+@Suppress("kotlin:S2245")
+class RandomizedSet {
+    private val list: MutableList<Int>
+    private val map: MutableMap<Int, Int>
+
+    /* Initialize your data structure here. */
+    init {
+        list = ArrayList()
+        map = HashMap()
+    }
+
+    /* Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    fun insert(`val`: Int): Boolean {
+        if (map.containsKey(`val`)) {
+            return false
+        }
+        map[`val`] = list.size
+        list.add(`val`)
+        return true
+    }
+
+    /* Removes a value from the set. Returns true if the set contained the specified element. */
+    fun remove(`val`: Int): Boolean {
+        if (!map.containsKey(`val`)) {
+            return false
+        }
+        val swap1 = map[`val`]!!
+        val swap2 = list.size - 1
+        val val2 = list[swap2]
+        map[val2] = swap1
+        map.remove(`val`)
+        list[swap1] = val2
+        list.removeAt(list.size - 1)
+        return true
+    }
+
+    /* Get a random element from the set. */
+    fun getRandom(): Int {
+        return list[Random.nextInt(list.size)]
+    }
+}
+
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = RandomizedSet()
+ * var param_1 = obj.insert(`val`)
+ * var param_2 = obj.remove(`val`)
+ * var param_3 = obj.getRandom()
+ */
