74982e5f39857ab627a4f8350a2689cba86b6c5c
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-07 09:48:26 +0200

    Time: 144 ms (84.62%), Space: 33.7 MB (69.23%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.kt
@@ -0,0 +1,48 @@
+import java.util.LinkedList
+import java.util.Queue
+
+class Solution {
+    private fun isInBank(set: Set<String>, cur: String): List<String> {
+        val res: MutableList<String> = ArrayList()
+        for (each in set) {
+            var diff = 0
+            for (i in 0 until each.length) {
+                if (each[i] != cur[i]) {
+                    diff++
+                    if (diff > 1) {
+                        break
+                    }
+                }
+            }
+            if (diff == 1) {
+                res.add(each)
+            }
+        }
+        return res
+    }
+
+    fun minMutation(start: String, end: String, bank: Array<String>): Int {
+        val set: MutableSet<String> = HashSet()
+        for (s in bank) {
+            set.add(s)
+        }
+        val queue: Queue<String> = LinkedList()
+        queue.offer(start)
+        var step = 0
+        while (!queue.isEmpty()) {
+            var curSize = queue.size
+            while (curSize-- > 0) {
+                val cur = queue.poll()
+                if (cur == end) {
+                    return step
+                }
+                for (next in isInBank(set, cur)) {
+                    queue.offer(next)
+                    set.remove(next)
+                }
+            }
+            step++
+        }
+        return -1
+    }
+}
