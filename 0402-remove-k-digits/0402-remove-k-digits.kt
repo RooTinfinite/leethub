131c910b99809c35f3776c940da6c889e72783d7
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-07 09:26:00 +0200

    Time: 183 ms (100.00%), Space: 37.3 MB (100.00%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun removeKdigits(num: String, k: Int): String {
+        var k = k
+        val list = CharArray(num.length)
+        val len = num.length - k
+        var top = 0
+        for (i in 0 until num.length) {
+            while (top > 0 && k > 0 && num[i] < list[top - 1]) {
+                top--
+                k--
+            }
+            list[top++] = num[i]
+        }
+        var number = 0
+        while (number < len && list[number] == '0') {
+            number++
+        }
+        return if (number == len) "0" else String(list, number, len - number)
+    }
+}
