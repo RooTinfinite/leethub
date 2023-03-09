bde0a1722c24c9fa071c8aa9d0b5717b7e17db6d
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-09 09:56:13 +0200

    Time: 622 ms (85.86%), Space: 65.6 MB (83.45%) - LeetHub

---
+++ 502-ipo/502-ipo.kt
@@ -0,0 +1,42 @@
+import java.util.PriorityQueue
+
+class Solution {
+    inner class Data(var profit: Int, var capital: Int)
+
+    // max heap for profit
+    var profitMaxHeap = PriorityQueue<Data> { d1, d2 ->
+        -1 * Integer.compare(
+            d1.profit,
+            d2.profit
+        )
+    }
+
+    // min heap for capital
+    var capitalMinHeap = PriorityQueue<Data> { d1, d2 -> Integer.compare(d1.capital, d2.capital) }
+    fun findMaximizedCapital(k: Int, w: Int, profits: IntArray, capital: IntArray): Int {
+        init(profits, capital)
+        var maxCapital = w
+        var currentCapital = w
+        for (i in 0 until k) {
+
+            // first fetch all tasks you can do with current capital and add those in profit max heap
+            while (!capitalMinHeap.isEmpty() && currentCapital >= capitalMinHeap.peek().capital) {
+                profitMaxHeap.add(capitalMinHeap.poll())
+            }
+
+            // if profit max heap is empty we can do nothing so break
+            if (profitMaxHeap.isEmpty()) break
+
+            // add profit to current capital and update maxCapital
+            currentCapital += profitMaxHeap.poll().profit
+            maxCapital = Math.max(maxCapital, currentCapital)
+        }
+        return maxCapital
+    }
+
+    private fun init(profits: IntArray, capital: IntArray) {
+        for (i in profits.indices) {
+            capitalMinHeap.add(Data(profits[i], capital[i]))
+        }
+    }
+}
