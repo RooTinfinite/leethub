232399eb2a45d4ab5b2cfa63d2de7a6ad78ab8ad
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-09 10:33:03 +0200

    Time: 155 ms (100.00%), Space: 35.7 MB (33.33%) - LeetHub

---
+++ 593-valid-square/593-valid-square.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun validSquare(p1: IntArray, p2: IntArray, p3: IntArray, p4: IntArray): Boolean {
+        val distancesSquared = IntArray(6)
+        distancesSquared[0] = getDistanceSquared(p1, p2)
+        distancesSquared[1] = getDistanceSquared(p1, p3)
+        distancesSquared[2] = getDistanceSquared(p1, p4)
+        distancesSquared[3] = getDistanceSquared(p2, p3)
+        distancesSquared[4] = getDistanceSquared(p2, p4)
+        distancesSquared[5] = getDistanceSquared(p3, p4)
+        distancesSquared.sort()
+        if (distancesSquared[0] == 0) {
+            return false
+        }
+        if (distancesSquared[0] != distancesSquared[3]) {
+            return false
+        }
+        return if (distancesSquared[4] != distancesSquared[5]) {
+            false
+        } else distancesSquared[5] == 2 * distancesSquared[0]
+    }
+
+    private fun getDistanceSquared(p1: IntArray, p2: IntArray): Int {
+        val deltaX = p2[0] - p1[0]
+        val deltaY = p2[1] - p1[1]
+        return deltaX * deltaX + deltaY * deltaY
+    }
+}
