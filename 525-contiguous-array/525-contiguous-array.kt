a509743be830bc86040eb40af37eeca0a8ace3fe
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-09 10:03:29 +0200

    Time: 503 ms (33.33%), Space: 45.2 MB (91.67%) - LeetHub

---
+++ 525-contiguous-array/525-contiguous-array.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun findMaxLength(nums: IntArray): Int {
+        for (i in nums.indices) {
+            if (nums[i] == 0) {
+                nums[i] = -1
+            }
+        }
+        val map: HashMap<Int, Int> = HashMap()
+        map[0] = -1
+        var ps = 0
+        var len = 0
+        for (i in nums.indices) {
+            ps += nums[i]
+            if (!map.containsKey(ps)) {
+                map[ps] = i
+            } else {
+                len = Math.max(len, i - map[ps]!!)
+            }
+        }
+        return len
+    }
+}
