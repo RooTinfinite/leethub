d824fbb21a6b72da57e57a14fc578becdb6b9add
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-09 10:33:18 +0200

    Time: 309 ms (100.00%), Space: 40.2 MB (61.90%) - LeetHub

---
+++ 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun findLHS(nums: IntArray): Int {
+        nums.sort()
+        var max = 0
+        var lastN = 0
+        var curN = 1
+        var cur = nums[0]
+        for (i in 1 until nums.size) {
+            if (nums[i] > cur) {
+                if (lastN > 0 && curN > 0 && lastN + curN > max) {
+                    max = lastN + curN
+                }
+                // if diff more than 1, don't count
+                lastN = if (nums[i] - cur == 1) curN else 0
+                curN = 1
+                cur = nums[i]
+            } else {
+                curN++
+            }
+        }
+        if (lastN > 0 && curN > 0 && lastN + curN > max) {
+            max = lastN + curN
+        }
+        return max
+    }
+}
