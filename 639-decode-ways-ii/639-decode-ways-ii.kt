34ed25792cfb49fee29ddcae69497c5f9b0e1bd9
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-10 10:13:37 +0200

    Time: 455 ms (33.33%), Space: 68.2 MB (33.33%) - LeetHub

---
+++ 639-decode-ways-ii/639-decode-ways-ii.kt
@@ -0,0 +1,45 @@
+class Solution {
+    fun numDecodings(s: String): Int {
+        if (s[0] == '0') {
+            return 0
+        }
+        val dp = LongArray(s.length + 1)
+        dp[0] = 1
+        dp[1] = (if (s[0] == '*') 9 else 1).toLong()
+        val ch = s.toCharArray()
+        for (i in 2..ch.size) {
+            if (ch[i - 1] == '0') {
+                if (ch[i - 2] == '1' || ch[i - 2] == '2') {
+                    dp[i] = dp[i - 2]
+                } else if (ch[i - 2] == '*') {
+                    dp[i] = 2 * dp[i - 2]
+                } else {
+                    return 0
+                }
+            } else if (ch[i - 1] >= '1' && ch[i - 1] <= '6') {
+                dp[i] = dp[i - 1]
+                if (ch[i - 2] == '1' || ch[i - 2] == '2') {
+                    dp[i] += dp[i - 2]
+                } else if (ch[i - 2] == '*') {
+                    dp[i] += 2 * dp[i - 2]
+                }
+            } else if (ch[i - 1] >= '7' && ch[i - 1] <= '9') {
+                dp[i] = dp[i - 1]
+                if (ch[i - 2] == '1' || ch[i - 2] == '*') {
+                    dp[i] += dp[i - 2]
+                }
+            } else if (ch[i - 1] == '*') {
+                dp[i] = 9 * dp[i - 1]
+                if (ch[i - 2] == '1') {
+                    dp[i] += 9 * dp[i - 2]
+                } else if (ch[i - 2] == '2') {
+                    dp[i] += 6 * dp[i - 2]
+                } else if (ch[i - 2] == '*') {
+                    dp[i] += 15 * dp[i - 2]
+                }
+            }
+            dp[i] = dp[i] % 1000000007
+        }
+        return dp[s.length].toInt()
+    }
+}
