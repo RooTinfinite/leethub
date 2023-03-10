31dfd13bd73502e6d7883d883b4bfc1c064e23b0
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-10 10:09:02 +0200

    Time: 400 ms (16.67%), Space: 43.5 MB (16.67%) - LeetHub

---
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.kt
@@ -0,0 +1,39 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun addOneRow(root: TreeNode?, `val`: Int, depth: Int): TreeNode? {
+        if (depth == 1) {
+            val newRoot = TreeNode(`val`)
+            newRoot.left = root
+            return newRoot
+        }
+        dfs(root!!, depth - 2, `val`)
+        return root
+    }
+
+    private fun dfs(node: TreeNode, depth: Int, `val`: Int) {
+        if (depth == 0) {
+            val left = TreeNode(`val`)
+            val right = TreeNode(`val`)
+            left.left = node.left
+            right.right = node.right
+            node.left = left
+            node.right = right
+        } else {
+            if (node.left != null) {
+                dfs(node.left!!, depth - 1, `val`)
+            }
+            if (node.right != null) {
+                dfs(node.right!!, depth - 1, `val`)
+            }
+        }
+    }
+}
