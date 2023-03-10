be10ddd8ef7c0f44500b108ff55c00ab3e7c57dd
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-10 10:11:47 +0200

    Time: 871 ms (11.11%), Space: 73.3 MB (11.11%) - LeetHub

---
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.kt
@@ -0,0 +1,45 @@
+import java.util.Objects
+import java.util.PriorityQueue
+
+class Solution {
+    internal class Triplet(var value: Int, var row: Int, var idx: Int) : Comparable<Triplet?> {
+        override operator fun compareTo(other: Triplet?): Int {
+            return value - other!!.value
+        }
+    }
+
+    fun smallestRange(nums: List<List<Int>>): IntArray {
+        val pq = PriorityQueue<Triplet>()
+        var maxInPq = Int.MIN_VALUE
+        for (i in nums.indices) {
+            pq.add(Triplet(nums[i][0], i, 0))
+            if (maxInPq < nums[i][0]) {
+                maxInPq = nums[i][0]
+            }
+        }
+        var rangeSize = maxInPq - Objects.requireNonNull(pq.peek()).value + 1
+        var rangeLeft = Objects.requireNonNull(pq.peek()).value
+        var rangeRight = maxInPq
+        while (true) {
+            val nextNumber = pq.remove()
+            if (nextNumber.idx + 1 < nums[nextNumber.row].size) {
+                val `val` = nums[nextNumber.row][nextNumber.idx + 1]
+                if (`val` > maxInPq) {
+                    maxInPq = `val`
+                }
+                pq.add(Triplet(`val`, nextNumber.row, nextNumber.idx + 1))
+                if (maxInPq - Objects.requireNonNull(pq.peek()).value + 1 < rangeSize) {
+                    rangeSize = maxInPq - pq.peek().value + 1
+                    rangeLeft = maxInPq
+                    rangeRight = pq.peek().value
+                }
+            } else {
+                break
+            }
+        }
+        val answer = IntArray(2)
+        answer[0] = rangeLeft
+        answer[1] = rangeRight
+        return answer
+    }
+}
