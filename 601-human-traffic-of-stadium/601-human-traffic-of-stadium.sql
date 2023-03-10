d4ae452cf21f0a7bdf84793eda1dab8b26d18a95
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-10 10:04:08 +0200

    Time: 683 ms (46.91%), Space: 0B (100.00%) - LeetHub

---
+++ 601-human-traffic-of-stadium/601-human-traffic-of-stadium.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+with new_group as(
+    select id,visit_date,people,id - row_number() over(order by id) as new
+    from Stadium
+    where people >= 100
+    )
+    select id,visit_date,people
+    from new_group
+    where new in(
+        select new
+        from new_group
+        group by new
+        having count(id) >= 3
+        );
