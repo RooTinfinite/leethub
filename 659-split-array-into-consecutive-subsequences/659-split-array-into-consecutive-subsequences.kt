1d4e438ecb63cd34e4eb21be57aeaea9f0eea113
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-12 07:51:23 +0200

    Time: 361 ms (83.33%), Space: 40.5 MB (50.00%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun isPossible(nums: IntArray): Boolean {
+        val element = IntArray(2001)
+        for (num in nums) {
+            element[num + 1000] += 1
+        }
+        for (i in element.indices) {
+            while (element[i] > 0) {
+                var length = 1
+                while (i + length < element.size &&
+                    element[i + length] >= element[i + length - 1]
+                ) {
+                    length += 1
+                }
+                if (length < 3) {
+                    return false
+                } else {
+                    for (j in i until i + length) {
+                        element[j] -= 1
+                    }
+                }
+            }
+        }
+        return true
+    }
+}
