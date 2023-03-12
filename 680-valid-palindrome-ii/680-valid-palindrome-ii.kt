82a8996591e51faeaeaf996c8ac9de56622c36a0
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-12 07:56:58 +0200

    Time: 265 ms (100.00%), Space: 36.1 MB (97.18%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun validPalindrome(s: String): Boolean {
+        var l = 0
+        var r = s.length - 1
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return isPalindrome(s.substring(l + 1, r + 1)) || isPalindrome(s.substring(l, r))
+            }
+            l++
+            r--
+        }
+        return true
+    }
+
+    private fun isPalindrome(s: String): Boolean {
+        var l = 0
+        var r = s.length - 1
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return false
+            }
+            l++
+            r--
+        }
+        return true
+    }
+}
