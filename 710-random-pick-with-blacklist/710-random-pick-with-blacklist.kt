125ddb38e1017f7de54ff6e9b4c0f6c8182375bf
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-12 13:45:37 +0200

    Time: 589 ms (100.00%), Space: 63.3 MB (83.33%) - LeetHub

---
+++ 710-random-pick-with-blacklist/710-random-pick-with-blacklist.kt
@@ -0,0 +1,37 @@
+import kotlin.random.Random
+
+class Solution(n: Int, blacklist: IntArray) {
+    private val map: MutableMap<Int, Int>
+    private val upperLimit: Int
+
+    init {
+        map = HashMap()
+        upperLimit = n - blacklist.size
+        for (`val` in blacklist) {
+            map[`val`] = -1
+        }
+        var j = n - 1
+        for (`val` in blacklist) {
+            if (`val` < upperLimit) {
+                while (map.containsKey(j)) {
+                    j--
+                }
+                map[`val`] = j
+                j--
+            }
+        }
+    }
+
+    fun pick(): Int {
+        val `val`: Int = Random.nextInt(upperLimit)
+        return if (map.containsKey(`val`)) {
+            map[`val`]!!
+        } else `val`
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = Solution(n, blacklist)
+ * var param_1 = obj.pick()
+ */
